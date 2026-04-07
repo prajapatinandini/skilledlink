2ba683936b865cd2a01d7296b4435fc94b84dd77
prajapatinandini <nandiniprajapati422@gmail.com>
2026-04-08 00:46:52 +0530

    Fixed assets path error for Vercel

--- sk_frontend/vite.config.js
+++ sk_frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // 👈 Yeh line Vercel par 404 aur MIME type errors ko fix karegi
 })
